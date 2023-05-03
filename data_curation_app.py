fe1d9a8f257590efd1be2de2e12d698392cdd4c8
Diana Mkhitaryan <diana@scopeo.ai>
2023-05-03 11:56:52 +0200

    Modified code to read and write files from AWS S3 bucket amend

--- data_curation_app.py
+++ data_curation_app.py
@@ -1,45 +1,48 @@
 import streamlit as st
 from streamlit_drawable_canvas import st_canvas
 import os
-import argparse
-import json
-from utils import get_image_file_name, read_document, resize_image, scale_bounding_box, save_current_state, handle_wrong_datapoint, handle_missing_datapoint, handle_user_choice
-import shutil
+from utils import get_image_file_name, read_document, resize_image, scale_bounding_box, save_current_state,\
+    handle_wrong_datapoint, handle_missing_datapoint, handle_user_choice, get_label_folder, \
+    list_files_in_folder, read_s3_file, move_file_s3, create_s3_folder
+import boto3
 
-parser = argparse.ArgumentParser()
-parser.add_argument("-i", "--input", dest="input", help="path of the input JSON file", required=True)
-parser.add_argument("-im", "--image", dest="image", help="path of invoice files", required=True)
-parser.add_argument("-o", "--output", dest="output", help="path of output files", required=True)
-args = parser.parse_args()
-OCR_results_path = args.input
-images_path = args.image
-output_path = args.output
+
+OCR_results_path = "max_predicted_data_to_curate/"
+images_path = "2240_invoices_supplier/"
+output_path = "results/"
+bucket = 'data-curation-application'
+client = boto3.client('s3')
 
 
 def next_page():
     if len(st.session_state["OCR_output_files"]) > 0:
         current_file = st.session_state["OCR_output_files"].pop(0)
         st.session_state["curation_output_files"].append(current_file)
-        shutil.move(os.path.join(st.session_state["label_folder_path"], current_file), os.path.join(st.session_state["output_folder_path"], current_file))
+        source_key = f"{st.session_state['label_folder_path']}/{current_file.split('/')[-1]}"
+        destination_key = f"{st.session_state['output_folder_path']}/{current_file.split('/')[-1]}"
+        move_file_s3(bucket, source_key, destination_key, client)
         st.session_state["previous_clicked"] = False
 
     st.experimental_rerun()
 
 
 def previous_page():
-    current_file = st.session_state["curation_output_files"].pop()
-    st.session_state["OCR_output_files"].insert(0, current_file)
-    shutil.move(os.path.join(st.session_state["output_folder_path"], current_file), os.path.join(st.session_state["label_folder_path"], current_file))
+    last_file = st.session_state["curation_output_files"].pop()
+    source_key = f"{st.session_state['output_folder_path']}/{last_file.split('/')[-1]}"
+    destination_key = f"{st.session_state['label_folder_path']}/{last_file.split('/')[-1]}"
+    move_file_s3(bucket, source_key, destination_key, client)
+    st.session_state["OCR_output_files"].insert(0, destination_key)
+
     st.session_state["previous_clicked"] = True
 
     st.experimental_rerun()
 
 
 def handle_image_and_bounding_box(OCR_results_file, images_path, bounding_boxes):
-    image_file_name = get_image_file_name(OCR_results_file, images_path)
+    image_file_name = get_image_file_name(OCR_results_file.split("/")[-1], images_path, bucket, client)
     if image_file_name is None:
         next_page()
-    image = read_document(image_file_name, images_path)
+    image = read_document(bucket, images_path+image_file_name, client)
     image, width_scale, height_scale = resize_image(image, 800, 800)
 
     bounding_boxes = scale_bounding_box(bounding_boxes, width_scale, height_scale)
@@ -75,18 +78,19 @@ if not st.session_state["initialized"]:
         unsafe_allow_html=True
     )
 
-    labels = os.listdir(OCR_results_path)
+    labels = get_label_folder(bucket, OCR_results_path, client)
     label = st.selectbox("Select the Label:", labels)
     if st.session_state["selected_label"] != label:
         st.session_state["selected_label"] = label
         st.session_state["label_folder_path"] = os.path.join(OCR_results_path, label)
-        list_files = [file for file in os.listdir(os.path.join(OCR_results_path, label)) if file.endswith(".json")]
+        list_files = [file for file in list_files_in_folder(bucket, st.session_state["label_folder_path"]) if file.endswith(".json")]
         list_files.sort()
         st.session_state["OCR_output_files"] = list_files
-        st.session_state["output_folder_path"] = os.path.join(output_path, label)
-        os.makedirs(st.session_state["output_folder_path"], exist_ok=True)
-        st.session_state["curation_output_files"] = [file for file in os.listdir(st.session_state["output_folder_path"])
+        st.session_state["output_folder_path"] = f"{output_path}{label}"
+        create_s3_folder(bucket, st.session_state["output_folder_path"], client)
+        st.session_state["curation_output_files"] = [file for file in list_files_in_folder(bucket, st.session_state["output_folder_path"])
                                                      if file.endswith(".json")]
+
     col3, col4 = st.columns([8, 1])
     if col4.button("Next"):
         st.session_state["initialized"] = True
@@ -97,14 +101,13 @@ elif len(st.session_state["OCR_output_files"]) == 0:
 
 elif "label_folder_path" in st.session_state:
     current_file = st.session_state["OCR_output_files"][0]
-    with open(os.path.join(st.session_state["label_folder_path"], current_file), "r") as file:
-        bounding_boxes = json.load(file)
+    bounding_boxes = read_s3_file(bucket, current_file, client)
     if len(bounding_boxes["objects"]) > 0 and bounding_boxes["user_reviewed"] == 1 \
             and not st.session_state["previous_clicked"]:
         next_page()
 
     st.set_page_config(layout="wide")
-
+    # show sidebar
     # add default width for sidebar
     st.markdown('''
             <style>
@@ -169,6 +172,26 @@ elif "label_folder_path" in st.session_state:
     if col1.button("Refresh image"):
         st.session_state["refresh_counter"] += 1
 
+    add_sidebar_empty_space(5)
+    col3, col4 = st.sidebar.columns([2, 5])
+
+    if col4.button("Wrong data point"):
+        save_current_state(current_file, handle_wrong_datapoint(bounding_boxes), bucket, client)
+        next_page()
+
+    if col4.button("Missing information"):
+        save_current_state(current_file, handle_missing_datapoint(bounding_boxes), bucket, client)
+        next_page()
+
+    add_sidebar_empty_space(5)
+    col5, col6 = st.sidebar.columns([5, 1])
+    if len(st.session_state.get("curation_output_files", [])) > 0:
+        if col5.button("Previous"):
+            previous_page()
+    if col6.button("Next"):
+        next_page()
+
+    #show image part
     canvas_result = st_canvas(
         background_image=image,
         display_toolbar=False,
@@ -180,28 +203,8 @@ elif "label_folder_path" in st.session_state:
         initial_drawing=bounding_boxes,
     )
     st.write(image_file_name)
-
-    add_sidebar_empty_space(5)
-    col3, col4 = st.sidebar.columns([2, 5])
-
-    if col4.button("Wrong data point"):
-        save_current_state(current_file, handle_wrong_datapoint(bounding_boxes), st.session_state["label_folder_path"])
-        next_page()
-
-    if col4.button("Missing information"):
-        save_current_state(current_file, handle_missing_datapoint(bounding_boxes), st.session_state["label_folder_path"])
-        next_page()
-
-    add_sidebar_empty_space(5)
-    col5, col6 = st.sidebar.columns([5, 1])
-    if len(st.session_state.get("curation_output_files", [])) > 0:
-        if col5.button("Previous"):
-            previous_page()
-    if col6.button("Next"):
-        next_page()
-
     if canvas_result.json_data is not None:
         any_dark_green_box, bounding_boxes = handle_user_choice(bounding_boxes, canvas_result.json_data["objects"])
         if any_dark_green_box:
-            save_current_state(current_file, bounding_boxes, st.session_state["label_folder_path"])
+            save_current_state(current_file, bounding_boxes, bucket, client)
             next_page()
--- utils.py
+++ utils.py
@@ -1,24 +1,72 @@
 from PIL import Image
 import json
-from pdf2image import convert_from_path
 import os
+import boto3
+import io
+from pdf2image import convert_from_bytes
 
 
-def get_image_file_name(json_file_name, image_path):
-    for image_file_name in os.listdir(image_path):
-        if os.path.splitext(image_file_name)[0] == json_file_name[2:-5]:
+def get_label_folder(bucket, OCR_results_path, client):
+    result = client.list_objects(Bucket=bucket, Prefix=OCR_results_path, Delimiter='/')
+    list_folders = []
+    for o in result.get('CommonPrefixes'):
+        list_folders.append(o.get('Prefix')[29:-1])
+    return list_folders
+
+
+def list_files_in_folder(bucket, folder_prefix):
+    s3 = boto3.resource('s3')
+    bucket_resource = s3.Bucket(bucket)
+
+    files = []
+    for obj in bucket_resource.objects.filter(Prefix=folder_prefix):
+        if obj.key != folder_prefix:
+            files.append(obj.key)
+
+    return files
+
+
+def read_s3_file(bucket, file_key, client):
+    obj = client.get_object(Bucket=bucket, Key=file_key)
+    content = obj['Body'].read().decode('utf-8')
+    json_content = json.loads(content)
+    return json_content
+
+
+def move_file_s3(bucket, source_key, destination_key, client):
+    client.copy_object(Bucket=bucket, CopySource={'Bucket': bucket, 'Key': source_key}, Key=destination_key)
+    client.delete_object(Bucket=bucket, Key=source_key)
+
+
+def create_s3_folder(bucket_name, folder_path, client):
+    if not folder_path.endswith('/'):
+        folder_path += '/'
+
+    client.put_object(Bucket=bucket_name, Key=folder_path)
+
+
+def get_image_file_name(json_file_name, image_path, bucket_name, client):
+    response = client.list_objects_v2(Bucket=bucket_name, Prefix=image_path)
+    for obj in response.get('Contents', []):
+        image_file_name = obj['Key'].split('/')[-1]
+
+        if image_file_name[:-4] == json_file_name[2:-5]:
             return image_file_name
 
 
 # TODO handle multiple pages of pdf
-def read_document(file_name, image_path):
-    file_extension = os.path.splitext(file_name)[-1].lower()
+def read_document(bucket, file_key, client):
+    obj = client.get_object(Bucket=bucket, Key=file_key)
+    file_data = obj['Body'].read()
+    file_extension = os.path.splitext(file_key)[-1].lower()
+
     if file_extension in ['.jpg', '.jpeg', '.png', '.heic', '.bmp', '.gif', '.tiff', '.tif', '.webp', '.heif']:
-        image = Image.open(os.path.join(image_path, file_name))
+        image = Image.open(io.BytesIO(file_data))
     elif file_extension == '.pdf':
-        image = convert_from_path(os.path.join(image_path, file_name))[0]
+        image = convert_from_bytes(file_data)[0]
     else:
         raise ValueError(f"Unsupported file format: {file_extension}")
+
     return image
 
 
@@ -47,21 +95,20 @@ def scale_bounding_box(bounding_boxes, width_scale, height_scale):
     return bounding_boxes
 
 
-def save_current_state(file_name, bounding_boxes, label_folder_path):
+def save_current_state(file_name, bounding_boxes, bucket, client):
+    obj = client.get_object(Bucket=bucket, Key=file_name)
+    saved_data = json.loads(obj['Body'].read().decode('utf-8'))
 
-    with open(os.path.join(label_folder_path, file_name), "r") as json_file:
-        saved_data = json.load(json_file)
-        saved_data["user_reviewed"] = bounding_boxes.get("user_reviewed", 0)
-        saved_data["missing_information"] = bounding_boxes.get("missing_information", False)
-        saved_data["wrong_datapoint"] = bounding_boxes.get("wrong_datapoint", False)
+    saved_data["user_reviewed"] = bounding_boxes.get("user_reviewed", 0)
+    saved_data["missing_information"] = bounding_boxes.get("missing_information", False)
+    saved_data["wrong_datapoint"] = bounding_boxes.get("wrong_datapoint", False)
 
     for idx, _ in enumerate(saved_data["objects"]):
         saved_data["objects"][idx]["result"] = bounding_boxes["objects"][idx].get("result", True)
         saved_data["objects"][idx]["first"] = bounding_boxes["objects"][idx].get("first", True)
         saved_data["objects"][idx]["last"] = bounding_boxes["objects"][idx].get("last", True)
-        
-    with open(os.path.join(label_folder_path, file_name), "w") as json_file:
-        json.dump(saved_data, json_file, indent=2)
+
+    client.put_object(Bucket=bucket, Key=file_name, Body=json.dumps(saved_data, indent=2).encode('utf-8'))
 
 
 def handle_wrong_datapoint(data):
